c7a412d8465e0f6bceedfa4733186772942b20e1
albertosk816 <albertoskater16@icloud.com>
2019-03-11 09:59:29 +0100

    copia2

--- src/datos/operarArray.java
+++ src/datos/operarArray.java
@@ -32,5 +32,20 @@ private static void imprimeArray() {
 		
 	}
 
+private static void imprimeArray() {
+	
+	
+	int num[] =  new int[10];
+	int suma = 0;
+	
+	for (int i = 0; i < num.length; i++) {
+		num[i] = (int) Math.round(Math.random()*20);
+		System.out.println(num[i]);
+		
+		suma = suma + num[i];
+
+	}
+	System.out.println("La suma de los valores del array es: " + suma);
+}
 
 }
