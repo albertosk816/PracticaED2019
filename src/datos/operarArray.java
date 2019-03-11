717260bdd143053716f36f1db1120eeeb667bc52
albertosk816 <albertoskater16@icloud.com>
2019-03-11 09:50:29 +0100

    principal

---
+++ src/datos/operarArray.java
@@ -0,0 +1,22 @@
+package datos;
+
+public class operarArray {
+
+	public static void main(String[] args) {
+		rellenaArray();
+
+	}
+
+	private static void rellenaArray() {
+		
+		
+		int num[] =  new int[10];
+		
+		for (int i = 0; i < num.length; i++) {
+			num[i] = (int) Math.round(Math.random()*20);
+			System.out.println(num[i]);
+		}
+		
+	}
+
+}
