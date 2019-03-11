360be6a6de3b73d8b5d65ad47610d99b196d6d84
albertosk816 <albertoskater16@icloud.com>
2019-03-11 09:57:25 +0100

    copia1

--- src/datos/operarArray.java
+++ src/datos/operarArray.java
@@ -4,6 +4,7 @@ public class operarArray {
 
 	public static void main(String[] args) {
 		rellenaArray();
+		imprimeArray();
 
 	}
 
@@ -18,5 +19,18 @@ public class operarArray {
 		}
 		
 	}
+	
+private static void imprimeArray() {
+		
+		
+		int num[] =  new int[10];
+		
+		for (int i = 0; i < num.length; i++) {
+			num[i] = (int) Math.round(Math.random()*20);
+			System.out.println(num[i]);
+		}
+		
+	}
+
 
 }
